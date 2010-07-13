9e58dc6780a3d221e1ac71812a4087d3c231f8b9
ashishgupta <ashishgupta@ae7b85dc-aa43-0410-b3f4-ca239e3fa864>
2010-07-13 20:55:50 +0000

    Fixed Configuration Files Due to Solver Name Change

--- Trunk/ParEuler3D/nbproject/Makefile-variables.mk
+++ Trunk/ParEuler3D/nbproject/Makefile-variables.mk
@@ -9,16 +9,16 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux-x86
 CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
-CND_ARTIFACT_NAME_Debug=euler3d
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/euler3d
+CND_ARTIFACT_NAME_Debug=pareuler3d
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/pareuler3d
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Debug=euler3d.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/euler3d.tar
+CND_PACKAGE_NAME_Debug=pareuler3d.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/pareuler3d.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
-CND_ARTIFACT_NAME_Release=euler3d
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/euler3d
+CND_ARTIFACT_NAME_Release=pareuler3d
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/pareuler3d
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Release=euler3d.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/euler3d.tar
+CND_PACKAGE_NAME_Release=pareuler3d.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/pareuler3d.tar
