8b9a2b08300ff9fef9289b9a883a7fb66378e051
Ashish Gupta <ashishgupta02@gmail.com>
2010-10-04 03:27:43 +0000

    Validation of Euler Code with Finite Difference Debuging tool

---
+++ Trunk/Euler2D/SOLVER/include/Euler2D_Solver_Finite_Difference.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   Euler2D_Solver_Finite_Difference.h
+ * Author: Ashish Gupta
+ *
+ * Created on October 3, 2010, 1:19 PM
+ */
+
+#ifndef _EULER2D_SOLVER_FINITE_DIFFERENCE_H
+#define	_EULER2D_SOLVER_FINITE_DIFFERENCE_H
+
+typedef struct {
+    double Resi_Old[4];
+    double Resi_New[4];
+} FD_NODE;
+
+#endif	/* _EULER2D_SOLVER_FINITE_DIFFERENCE_H */
+
