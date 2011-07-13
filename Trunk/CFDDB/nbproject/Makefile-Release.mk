7b400bf58d399635cdb218fcc0f47aa233939732
ashishgupta <ashishgupta@ae7b85dc-aa43-0410-b3f4-ca239e3fa864>
2011-07-13 20:03:07 +0000

    Revision 4: Sync Utilities Code

--- Trunk/CFDDB/nbproject/Makefile-Release.mk
+++ Trunk/CFDDB/nbproject/Makefile-Release.mk
@@ -10,6 +10,8 @@
 # Environment
 MKDIR=mkdir
 CP=cp
+GREP=grep
+NM=nm
 CCADMIN=CCadmin
 RANLIB=ranlib
 CC=gcc
@@ -22,25 +24,27 @@ AS=as
 CND_PLATFORM=GNU-Linux-x86
 CND_CONF=Release
 CND_DISTDIR=dist
+CND_BUILDDIR=build
 
 # Include project Makefile
 include Makefile
 
 # Object Directory
-OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/src/DBNETCDF.o \
-	${OBJECTDIR}/src/DBERROR.o \
-	${OBJECTDIR}/src/DBCGNS.o \
-	${OBJECTDIR}/src/DB.o \
-	${OBJECTDIR}/src/cgnsIO.o \
-	${OBJECTDIR}/src/DBMANAGER.o \
 	${OBJECTDIR}/src/ugridIO.o \
+	${OBJECTDIR}/src/DBMANAGER.o \
+	${OBJECTDIR}/src/netcdfIO.o \
+	${OBJECTDIR}/src/DB.o \
 	${OBJECTDIR}/src/DBUGRID.o \
 	${OBJECTDIR}/src/corelib.o \
-	${OBJECTDIR}/src/netcdfIO.o
+	${OBJECTDIR}/src/DBNETCDF.o \
+	${OBJECTDIR}/src/DBERROR.o \
+	${OBJECTDIR}/src/cgnsIO.o \
+	${OBJECTDIR}/src/DBCGNS.o
+
 
 # C Compiler Flags
 CFLAGS=
@@ -60,63 +64,63 @@ LDLIBSOPTIONS=-L../../../../00-Applications/HPC_LIBS/lib -lcgns -lnetcdf -Wl,-rp
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	${MAKE}  -f nbproject/Makefile-Release.mk dist/Release/GNU-Linux-x86/libCFDDB.so
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libCFDDB.so
 
-dist/Release/GNU-Linux-x86/libCFDDB.so: ../UTILS/dist/Release/GNU-Linux-x86/libUTILS.so
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libCFDDB.so: ../UTILS/dist/Release/GNU-Linux-x86/libUTILS.so
 
-dist/Release/GNU-Linux-x86/libCFDDB.so: ${OBJECTFILES}
-	${MKDIR} -p dist/Release/GNU-Linux-x86
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libCFDDB.so: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -shared -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libCFDDB.so -fPIC ${OBJECTFILES} ${LDLIBSOPTIONS} 
 
-${OBJECTDIR}/src/DBNETCDF.o: nbproject/Makefile-${CND_CONF}.mk src/DBNETCDF.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DBNETCDF.o src/DBNETCDF.cpp
-
-${OBJECTDIR}/src/DBERROR.o: nbproject/Makefile-${CND_CONF}.mk src/DBERROR.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DBERROR.o src/DBERROR.cpp
-
-${OBJECTDIR}/src/DBCGNS.o: nbproject/Makefile-${CND_CONF}.mk src/DBCGNS.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DBCGNS.o src/DBCGNS.cpp
-
-${OBJECTDIR}/src/DB.o: nbproject/Makefile-${CND_CONF}.mk src/DB.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DB.o src/DB.cpp
-
-${OBJECTDIR}/src/cgnsIO.o: nbproject/Makefile-${CND_CONF}.mk src/cgnsIO.c 
-	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.c) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/cgnsIO.o src/cgnsIO.c
-
-${OBJECTDIR}/src/DBMANAGER.o: nbproject/Makefile-${CND_CONF}.mk src/DBMANAGER.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DBMANAGER.o src/DBMANAGER.cpp
-
-${OBJECTDIR}/src/ugridIO.o: nbproject/Makefile-${CND_CONF}.mk src/ugridIO.c 
+${OBJECTDIR}/src/ugridIO.o: src/ugridIO.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
 	$(COMPILE.c) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/ugridIO.o src/ugridIO.c
 
-${OBJECTDIR}/src/DBUGRID.o: nbproject/Makefile-${CND_CONF}.mk src/DBUGRID.cpp 
+${OBJECTDIR}/src/DBMANAGER.o: src/DBMANAGER.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DBMANAGER.o src/DBMANAGER.cpp
+
+${OBJECTDIR}/src/netcdfIO.o: src/netcdfIO.c 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.c) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/netcdfIO.o src/netcdfIO.c
+
+${OBJECTDIR}/src/DB.o: src/DB.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DB.o src/DB.cpp
+
+${OBJECTDIR}/src/DBUGRID.o: src/DBUGRID.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DBUGRID.o src/DBUGRID.cpp
 
-${OBJECTDIR}/src/corelib.o: nbproject/Makefile-${CND_CONF}.mk src/corelib.c 
+${OBJECTDIR}/src/corelib.o: src/corelib.c 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
 	$(COMPILE.c) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/corelib.o src/corelib.c
 
-${OBJECTDIR}/src/netcdfIO.o: nbproject/Makefile-${CND_CONF}.mk src/netcdfIO.c 
+${OBJECTDIR}/src/DBNETCDF.o: src/DBNETCDF.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.c) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/netcdfIO.o src/netcdfIO.c
+	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DBNETCDF.o src/DBNETCDF.cpp
+
+${OBJECTDIR}/src/DBERROR.o: src/DBERROR.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DBERROR.o src/DBERROR.cpp
+
+${OBJECTDIR}/src/cgnsIO.o: src/cgnsIO.c 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.c) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/cgnsIO.o src/cgnsIO.c
+
+${OBJECTDIR}/src/DBCGNS.o: src/DBCGNS.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Wall -Iinclude -I../UTILS/include -I../../../../00-Applications/HPC_LIBS/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/DBCGNS.o src/DBCGNS.cpp
 
 # Subprojects
 .build-subprojects:
@@ -125,8 +129,8 @@ ${OBJECTDIR}/src/netcdfIO.o: nbproject/Makefile-${CND_CONF}.mk src/netcdfIO.c
 
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
-	${RM} -r build/Release
-	${RM} dist/Release/GNU-Linux-x86/libCFDDB.so
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libCFDDB.so
 
 # Subprojects
 .clean-subprojects:
