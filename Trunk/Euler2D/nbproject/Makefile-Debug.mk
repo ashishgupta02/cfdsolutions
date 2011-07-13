4d56ee0ad99a10c19a3fd3f4edb9a09fb5937958
Ashish Gupta <ashishgupta02@gmail.com>
2011-07-13 20:49:10 +0000

    Revision 4: Sync Utilities Code

--- Trunk/Euler2D/nbproject/Makefile-Debug.mk
+++ Trunk/Euler2D/nbproject/Makefile-Debug.mk
@@ -18,18 +18,19 @@ CC=gcc
 CCC=g++
 CXX=g++
 FC=gfortran
-AS=
+AS=as
 
 # Macros
 CND_PLATFORM=GNU-Linux-x86
 CND_CONF=Debug
 CND_DISTDIR=dist
+CND_BUILDDIR=build
 
 # Include project Makefile
 include Makefile
 
 # Object Directory
-OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
@@ -66,12 +67,12 @@ LDLIBSOPTIONS=-lm -Wl,-rpath ../UTILS/dist/Debug/GNU-Linux-x86 -L../UTILS/dist/D
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-Debug.mk dist/Debug/GNU-Linux-x86/euler2d
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/euler2d
 
-dist/Debug/GNU-Linux-x86/euler2d: ../UTILS/dist/Debug/GNU-Linux-x86/libUTILS.so
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/euler2d: ../UTILS/dist/Debug/GNU-Linux-x86/libUTILS.so
 
-dist/Debug/GNU-Linux-x86/euler2d: ${OBJECTFILES}
-	${MKDIR} -p dist/Debug/GNU-Linux-x86
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/euler2d: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	g++ -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/euler2d ${OBJECTFILES} ${LDLIBSOPTIONS} 
 
 ${OBJECTDIR}/SOLVER/src/Euler2D_Solver_VanLeer.o: SOLVER/src/Euler2D_Solver_VanLeer.cpp 
@@ -146,8 +147,8 @@ ${OBJECTDIR}/SOLVER/src/Euler2D_Solver_LDFSS.o: SOLVER/src/Euler2D_Solver_LDFSS.
 
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
-	${RM} -r build/Debug
-	${RM} dist/Debug/GNU-Linux-x86/euler2d
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/euler2d
 
 # Subprojects
 .clean-subprojects:
--- Trunk/Euler2D/nbproject/Makefile-variables.mk
+++ Trunk/Euler2D/nbproject/Makefile-variables.mk
@@ -22,3 +22,14 @@ CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/euler2d
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=euler2d.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/euler2d.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
