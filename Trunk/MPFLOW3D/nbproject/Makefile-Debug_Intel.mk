66e705254f94126031fe03d688735df6cef590b5
Ashish Gupta <ashishgupta02@gmail.com>
2013-04-02 00:17:12 +0000

    Minor Configuration Change

--- Trunk/MPFLOW3D/nbproject/Makefile-Debug_Intel.mk
+++ Trunk/MPFLOW3D/nbproject/Makefile-Debug_Intel.mk
@@ -70,7 +70,6 @@ OBJECTFILES= \
 	${OBJECTDIR}/src/StegerWarming_Fluxes.o \
 	${OBJECTDIR}/src/Test.o \
 	${OBJECTDIR}/src/Time_Step.o \
-	${OBJECTDIR}/src/Tmp.o \
 	${OBJECTDIR}/src/VanLeer_Fluxes.o
 
 
@@ -276,11 +275,6 @@ ${OBJECTDIR}/src/Time_Step.o: src/Time_Step.cpp
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
 	$(COMPILE.cc) -g -Wall -DHAVE_MPI -I.. -I../UTILS/include -I../MATH/include -Iinclude -I/home/agupta/00-Applications/Lakesite/HPC_LIBS/include -axSSE4.2 -Wno-write-strings -fno-math-errno -fstrict-aliasing -fomit-frame-pointer -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Time_Step.o src/Time_Step.cpp
-
-${OBJECTDIR}/src/Tmp.o: src/Tmp.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -DHAVE_MPI -I.. -I../UTILS/include -I../MATH/include -Iinclude -I/home/agupta/00-Applications/Lakesite/HPC_LIBS/include -axSSE4.2 -Wno-write-strings -fno-math-errno -fstrict-aliasing -fomit-frame-pointer -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Tmp.o src/Tmp.cpp
 
 ${OBJECTDIR}/src/VanLeer_Fluxes.o: src/VanLeer_Fluxes.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
