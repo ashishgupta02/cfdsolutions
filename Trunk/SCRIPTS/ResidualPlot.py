1d51b703feb8cc5066e7d1894c4042e27f562542
ashishgupta <ashishgupta@ae7b85dc-aa43-0410-b3f4-ca239e3fa864>
2013-09-18 21:28:40 +0000

    Bug fix for Plotter

--- Trunk/SCRIPTS/ResidualPlot.py
+++ Trunk/SCRIPTS/ResidualPlot.py
@@ -4,11 +4,12 @@
 from numpy import *
 
 # If the package has been installed correctly, this should work:
-import Gnuplot, Gnuplot.funcutils
-import os, sys, time
+import Gnuplot
+import os
+import sys
+import time
 import signal
 from optparse import OptionParser
-
 
 #-----------------------------------------------------
 def signal_handler(signal, frame):
@@ -16,17 +17,17 @@ def signal_handler(signal, frame):
 
 #-----------------------------------------------------
 def VariableID(var):
-    if (var.lower() == "rho"):
+    if var.lower() == "rho":
         return 2
-    if (var.lower() == "rhou"):
+    if var.lower() == "rhou":
         return 3
-    if (var.lower() == "rhov"):
+    if var.lower() == "rhov":
         return 4
-    if (var.lower() == "rhow"):
+    if var.lower() == "rhow":
         return 5
-    if (var.lower() == "rhoet"):
+    if var.lower() == "rhoet":
         return 6
-    if (var.lower() == "res"):
+    if var.lower() == "res":
         return 7
 
 #-----------------------------------------------------
@@ -38,22 +39,22 @@ def ResidualPlot(g, filename, variable):
     else:
         iDim = 2
         jDim = 3
-    # Get the Modified Time Stamp of the file
+        # Get the Modified Time Stamp of the file
     g('set style function lines')
     g('set size 1.0, 1.0')
     g('set origin 0.0, 0.0')
     g('set multiplot')
     for p in range(length):
-        size1 = 1.0/float(iDim)
-        size2 = 1.0/float(jDim)        
+        size1 = 1.0 / float(iDim)
+        size2 = 1.0 / float(jDim)
         string = 'set size ' + str(size1) + ', ' + str(size2)
         g(string)
         if p < 3:
             origin1 = 0.0
-            origin2 = p*(1.0/float(jDim))
+            origin2 = p * (1.0 / float(jDim))
         else:
-            origin1 = 1.0/float(iDim)
-            origin2 = (p-3)*(1.0/float(jDim))  
+            origin1 = 1.0 / float(iDim)
+            origin2 = (p - 3) * (1.0 / float(jDim))
         string = 'set origin ' + str(origin1) + ', ' + str(origin2)
         g(string)
         g('set grid')
@@ -64,14 +65,14 @@ def ResidualPlot(g, filename, variable):
         string = variable[p]
         g.ylabel(string)
         g('set logscale y')
-        string = '"' + filename + '" using 1:' + str(VariableID(variable[p])) + ' ls ' + str(p+1)
+        string = '"' + filename + '" using 1:' + str(VariableID(variable[p])) + ' ls ' + str(p + 1)
         g.plot(string)
     g('unset multiplot')
 
 #-----------------------------------------------------
 def LoopPlot(filename, variable):
     # Get the Modified Time Stamp of the file
-    mtime1 = os.stat(filename).st_mtime;    
+    mtime1 = os.stat(filename).st_mtime;
     g = Gnuplot.Gnuplot(debug=0)
     ResidualPlot(g, filename, variable)
     while True:
@@ -88,22 +89,22 @@ def main():
     usage = "usage: %prog -f Filename -v Variable -p"
     parser = OptionParser(usage=usage)
     parser.add_option("-f", "--file", action="store", type="string", dest="filename", default="residual.res")
-    parser.add_option("-v", "--var",  action="append", type="string", dest="variable")
+    parser.add_option("-v", "--var", action="append", type="string", dest="variable")
     (options, args) = parser.parse_args()
-    
-    if options.filename == None:
-        print "ERROR: Input File Not Provided !" 
+
+    if options.filename is None:
+        print "ERROR: Input File Not Provided !"
         sys.exit()
-    
-    if options.variable == None:
-        print "ERROR: No variable Supplied to Parse !" 
+
+    if options.variable is None:
+        print "ERROR: No variable Supplied to Parse !"
         sys.exit()
-    
+
     # Check if File exits
     if not os.path.exists(options.filename):
         print "ERROR: Input File Does Not Exists !"
         sys.exit()
-    
+
     # Check if Parameter file exists
     LoopPlot(options.filename, options.variable)
     return
